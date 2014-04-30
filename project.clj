b40809abda3d8f7e163478713bf807189645a9df
Laszlo Toeroek <ltorokjr@gmail.com>
2014-04-30 12:56:24 +0100

    update dependencies (clj 1.6, bc 1.50, remove c.t.logging), add type hints

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-crypto "1.0.1-SNAPSHOT"
+(defproject laczoka/clj-crypto "1.1"
   :description "Clj-crypto is a wrapper for Bouncy Castle which allows you to easily use cryptography in your clojure app."
-  :dependencies [[bouncycastle/bcprov-jdk16 "140"]
+  :global-vars {*warn-on-reflection* true}
+  :dependencies [[org.bouncycastle/bcprov-jdk15on "1.50"]
                  [commons-codec/commons-codec "1.5"]
-                 [org.clojure/clojure "1.2.1"]
-                 [org.clojure/tools.logging "0.2.0"]])
+                 [org.clojure/clojure "1.6.0"]])
--- src/clj_crypto/core.clj
+++ src/clj_crypto/core.clj
@@ -1,13 +1,13 @@
 (ns clj-crypto.core
   (:refer-clojure :exclude [format])
-  (:require [clojure.tools.logging :as logging])
   (:import [org.apache.commons.codec.binary Base64]
            [org.bouncycastle.jce.provider BouncyCastleProvider]
-           [java.security KeyFactory KeyPair KeyPairGenerator MessageDigest PrivateKey PublicKey Security Signature]
+           [java.security Key KeyFactory KeyPair KeyPairGenerator MessageDigest PrivateKey PublicKey Security Signature]
            [java.security.spec PKCS8EncodedKeySpec X509EncodedKeySpec]
            [java.util Random]
            [javax.crypto Cipher SecretKeyFactory]
-           [javax.crypto.spec SecretKeySpec]))
+           [javax.crypto.spec SecretKeySpec]
+           [java.nio ByteBuffer]))
 
 
 (def des-algorithm "DES")
@@ -15,22 +15,32 @@
 (def default-symmetrical-algorithm des-algorithm)
 
 (def default-algorithm "RSA")
-(def default-signature-algorithm "SHA1withRSA")
+(def ^:dynamic default-signature-algorithm "SHA1withRSA")
 (def default-transformation "RSA/None/NoPadding")
-(def default-provider "BC") ; Bouncy Castle provider.
-(def default-character-encoding "UTF8")
+(def ^String default-provider "BC") ; Bouncy Castle provider.
+(def ^String default-character-encoding "UTF8")
 
 (def default-encrypt-password-algorithm "SHA-256")
 (def default-encrypt-password-n 1000)
 
 (Security/addProvider (new BouncyCastleProvider))
 
+
+(defn encode-base64 [bindata]
+  (Base64/encodeBase64 bindata))
+
+(defn encode-base64-as-str [bindata]
+  (Base64/encodeBase64String bindata))
+
+(defn decode-base64 [^String base64str]
+  (Base64/decodeBase64 base64str))
+
 (defn generate-key-pair []
   (let [key-pair-generator (KeyPairGenerator/getInstance default-algorithm)]
      (.initialize key-pair-generator 1024)
      (.generateKeyPair key-pair-generator)))
 
-(defn private-key [key-pair]
+(defn private-key [^KeyPair key-pair]
   (.getPrivate key-pair))
 
 (defn as-private-key [key]
@@ -39,70 +49,76 @@
     (instance? PrivateKey key) key
     true (throw (RuntimeException. (str "Don't know how to convert to private key: " key)))))
 
-(defn public-key [key-pair]
+(defn public-key ^PublicKey [^KeyPair key-pair]
   (.getPublic key-pair))
 
-(defn as-public-key [key]
+(defn as-public-key ^PublicKey [key]
   (cond
     (instance? KeyPair key) (public-key key)
     (instance? PublicKey key) key
     true (throw (RuntimeException. (str "Don't know how to convert to public key: " key)))))
 
-(defn algorithm [key]
+(defn algorithm [^Key key]
   (.getAlgorithm key))
 
-(defn encoded [key]
+(defn encoded [^Key key]
   (.getEncoded key))
 
-(defn format [key]
+(defn format [^Key key]
   (.getFormat key))
 
-(defn create-cipher 
+(defn create-cipher
   ([] (create-cipher default-transformation default-provider))
-  ([transformation] (create-cipher transformation default-provider))
-  ([transformation provider]
+  ([^String transformation] (create-cipher transformation default-provider))
+  ([^String transformation ^String provider]
     (Cipher/getInstance transformation provider)))
 
-(defn integer-byte [integer byte-offset]
+(defn ^byte integer-byte [integer byte-offset]
   (let [short-int (bit-and 0xff (bit-shift-right integer (* byte-offset 8)))]
     (if (< short-int 128)
       (byte short-int)
       (byte (- short-int 256)))))
 
-(defn integer-bytes [integer]
+(defn ^bytes integer-bytes [integer]
   (byte-array [(integer-byte integer 3) (integer-byte integer 2) (integer-byte integer 1) (integer-byte integer 0)]))
 
-(defn get-data-bytes [data]
+(defn ^bytes long-bytes [l]
+  (let [^ByteBuffer buf (ByteBuffer/allocate (/ Long/SIZE 8))]
+    (.putLong buf l)
+    (.array buf)))
+
+(defn get-data-bytes ^bytes [data]
   (cond
-    (instance? String data) (.getBytes data default-character-encoding)
-    (instance? Integer data) (integer-bytes data)
+   (instance? String data) (let [^String data data] (.getBytes data default-character-encoding))
+   (instance? Integer data) (integer-bytes data)
+   (instance? Long data) (long-bytes data)
     true data))
 
 (defn get-data-str [data]
   (if (instance? String data)
     data
-    (String. data default-character-encoding)))
+    (String. ^bytes data default-character-encoding)))
 
 (defn get-encrypt-key [key]
   (if (instance? KeyPair key)
-    (.getPublic key)
+    (.getPublic ^KeyPair key)
     key))
 
-(defn do-cipher [cipher mode key data]
-  (.init cipher mode key)
+(defn do-cipher [^Cipher cipher mode ^Key key data]
+  (.init cipher ^int mode key)
   (.doFinal cipher (get-data-bytes data)))
 
-(defn encrypt 
+(defn encrypt
   ([key data] (encrypt key data (create-cipher)))
   ([key data cipher]
     (do-cipher cipher Cipher/ENCRYPT_MODE (get-encrypt-key key) data)))
 
 (defn get-decrypt-key [key]
   (if (instance? KeyPair key)
-    (.getPrivate key)
+    (.getPrivate ^KeyPair key)
     key))
 
-(defn decrypt 
+(defn decrypt
   ([key data] (decrypt key data (create-cipher)))
   ([key data cipher]
     (get-data-str (do-cipher cipher Cipher/DECRYPT_MODE (get-decrypt-key key) data))))
@@ -110,16 +126,16 @@
 ; Save and Load keypairs
 
 (defn get-public-key-map [public-key]
-  { :algorithm (.getAlgorithm public-key)
-    :bytes (.getEncoded (X509EncodedKeySpec. (.getEncoded public-key))) })
+  { :algorithm (.getAlgorithm ^PublicKey public-key)
+    :bytes (.getEncoded (X509EncodedKeySpec. (.getEncoded ^PublicKey public-key))) })
 
 (defn get-private-key-map [private-key]
-  { :algorithm (.getAlgorithm private-key)
-    :bytes (.getEncoded (PKCS8EncodedKeySpec. (.getEncoded private-key))) })
+  { :algorithm (.getAlgorithm ^PrivateKey private-key)
+    :bytes (.getEncoded (PKCS8EncodedKeySpec. (.getEncoded ^PrivateKey private-key))) })
 
 (defn get-key-pair-map [key-pair]
-  { :public-key (get-public-key-map (.getPublic key-pair))
-    :private-key (get-private-key-map (.getPrivate key-pair))})
+  { :public-key (get-public-key-map (.getPublic ^KeyPair key-pair))
+    :private-key (get-private-key-map (.getPrivate ^KeyPair key-pair))})
 
 (defn decode-public-key [public-key-map]
   (when public-key-map
@@ -138,14 +154,14 @@
 
 (defn sign [key data]
   (let [private-key (as-private-key key)
-        signature (Signature/getInstance default-signature-algorithm default-provider)]
+        signature (Signature/getInstance ^String default-signature-algorithm ^String default-provider)]
     (.initSign signature private-key)
     (.update signature (get-data-bytes data))
     (.sign signature)))
 
 (defn verify-signature [key data signature]
   (let [public-key (as-public-key key)
-        signature-obj (Signature/getInstance default-signature-algorithm default-provider)]
+        signature-obj (Signature/getInstance ^String default-signature-algorithm ^String default-provider)]
     (.initVerify signature-obj public-key)
     (.update signature-obj (get-data-bytes data))
     (.verify signature-obj (get-data-bytes signature))))
